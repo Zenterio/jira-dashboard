6e69b2db842e677f011a746d8dd833178287f9cc
Per Böhlin <per.bohlin@zenterio.com>
2017-05-28 06:53:56 +0200

    Improved README and corrected indentation in js-file.

--- README.md
+++ README.md
@@ -1,14 +1,50 @@
-# Zenterio Jira Description Template
+# Zenterio Jira Dashboard
 
-The solution is based on using jquery to manipulate the layout to strip-away the
-header, footer and dashboard side-panel.
+Jira is missing a good *dashboard* view for monitoring and information display
+on permanent screen/TV installations. The standard dashboard has a lot of
+unnecessary information such as the dashboard side-panel, menus, headers and
+footers.
+
+The built-in wallboard is slow, hard to use and customize.
+
+This solution is based on using JavaScript (jquery) to manipulate the layout of
+a standard dashboard, to strip-away menus, header, footer and dashboard side-panel.
+
+It has been implemented for **Atlassian Jira 7.3.1** and might require some
+tweaking for other versions.
 
 ## License
 
-See LICENSE.md
+MIT, see LICENSE.md.
 
 ## Installation
 
-Add the code using a script-tag in the announcement banner or serve the file
-from the same domain as your jira-server and include the file using a script tag
-in the announcement banner.
+The simplest way to integrate custom JavaScript in jira is to place it in the
+*announcement banner*. It can also be added in page headers and footers, but
+it usually require more work and doesn't survive jira-upgrades. Consult the
+Atlassian jira manual for how to integrate custom JavaScript that best suites
+your needs.
+
+### Announcement Banner
+
+In the announcement banner, place a script-tag, either with the content of
+zenterio.dashboard.js, or a reference to it. If referencing it using the
+`src` attribute, it is important that it is served from the same domain as your
+jira server, to allow proper DOM manipulation in all browsers.
+
+inline script:
+
+    <script type="text/javascript">
+    // content of zenterio.dashboard.js
+    </script>
+
+sourced script:
+
+    <script type="text/javascript" src="zenterio.dashboard.js"></script>
+
+## Activation
+
+To activate the dashboard mode, add the following to the search-query section
+of the URL.
+
+    ?zenterioDashboardMode=true
--- zenterio.dashboard.js
+++ zenterio.dashboard.js
@@ -14,68 +14,70 @@
      */
     function getQueryStringParams() {
 
-	var searchStr = window.location.search
-	// If search string not set, empty search string.
-	if (typeof searchStr === 'undefined') {
-	    searchStr = "?";
-	}
-	var match;
-	var pl = /\+/g;  // Regex for replacing addition symbol with a space
-	var search = /([^&=]+)=?([^&]*)/g;
-	var decode = function(s) {
-	    return decodeURIComponent(s.replace(pl, " "));
-	};
-	var query = searchStr.substring(1);
-	var result = {};
-	var key = "";
-	var value = "";
+        var searchStr = window.location.search
+        // If search string not set, empty search string.
+        if (typeof searchStr === 'undefined') {
+            searchStr = "?";
+        }
+        var match;
+        var pl = /\+/g;  // Regex for replacing addition symbol with a space
+        var search = /([^&=]+)=?([^&]*)/g;
+        var decode = function(s) {
+            return decodeURIComponent(s.replace(pl, " "));
+        };
+        var query = searchStr.substring(1);
+        var result = {};
+        var key = "";
+        var value = "";
 
-	while ((match = search.exec(query))) {
-	    key = decode(match[1]);
-	    value = decode(match[2]);
-	    value = convertURLValue(value)
-	    if (key in result) {
-		var oldValue = result[key];
-		if (oldValue instanceof Array) {
-		    oldValue.push(value);
-		} else {
-		    result[key] = [oldValue, value];
-		}
-	    } else {
-		result[key] = value;
-	    }
-
-	}
-	return result;
+        while ((match = search.exec(query))) {
+            key = decode(match[1]);
+            value = decode(match[2]);
+            value = convertURLValue(value)
+            if (key in result) {
+                var oldValue = result[key];
+                if (oldValue instanceof Array) {
+                    oldValue.push(value);
+                } else {
+                    result[key] = [oldValue, value];
+                }
+            } else {
+                result[key] = value;
+            }
+        }
+        return result;
     }
 
     function convertURLValue(value) {
-	switch (value.toLowerCase()) {
-	case "false":
-	    return false;
-	case "true":
-	    return true;
-	}
-	if (! isNaN(value)) {
-	    return +value // Converts to a number
-	}
-	return value
+        switch (value.toLowerCase()) {
+            case "false":
+                return false;
+            case "true":
+                return true;
+        }
+        if (! isNaN(value)) {
+            return +value // Converts to a number
+        }
+        return value
     }
 
-    function dashBoardMode(e) {
-	AJS.$('#footer').hide();
-	AJS.$('#header').hide();
-	AJS.$('#announcement-banner').hide();
-	AJS.$('div#dashboard').find('ul.dashboard-tabs').hide();
-	AJS.$('div#dashboard').css('margin-left', '0px');
-	AJS.$('div#dashboard-content').css('margin-left', '0px');
-	AJS.$('div#dashboard-content').find('div.aui-page-header').hide();
-	AJS.$('div#dashboard-content').css('margin-top', '10px');
+    /**
+     * Manipulates the DOM to strip away unwanted sections.
+     */
+    function activateDashBoardMode(e) {
+        AJS.$('#footer').hide();
+        AJS.$('#header').hide();
+        AJS.$('#announcement-banner').hide();
+        AJS.$('div#dashboard').find('ul.dashboard-tabs').hide();
+        AJS.$('div#dashboard').css('margin-left', '0px');
+        AJS.$('div#dashboard-content').css('margin-left', '0px');
+        AJS.$('div#dashboard-content').find('div.aui-page-header').hide();
+        AJS.$('div#dashboard-content').css('margin-top', '10px');
     }
-    
+
     var cfg = getQueryStringParams();
     if (cfg.zenterioDashboardMode == true) {
-	AJS.$(document).ready(dashBoardMode);
+        AJS.$(document).ready(activateDashBoardMode);
     }
-    
+
 })();
